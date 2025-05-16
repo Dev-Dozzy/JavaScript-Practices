7c9a4855e2f7af9a5a8fe6ad84addb99e3ef9640
Dev-Dozzy <chukwuma.chiedoziepre@gmail.com>
2025-05-16 09:44:58 +0100

    Worked on Variables and console logging them

---
+++ script.js
@@ -0,0 +1,63 @@
+// let fruits = ["apple", "banana", "orange"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+// let digits = [1, 5, 7, 6, 8, 9];
+// let a = 0;
+// for (i = 0; i < 6; i++) {
+//   a = a + digits[i];
+// }
+// console.log(a);
+
+// let user = {
+//   name: 'Alice',
+//   age: 25,
+//   email: 'alice@example.com',
+//   school: function() {
+//     return("I am a student.");
+//   }
+// }
+
+// console.log(user.school())
+
+// let book = {
+//   title: 'The Richest Man in Babylon',
+//   author: 'George Samuel Clason',
+//   yearPublished: "1926",
+//   isAvailable: true,
+//   borrowBook: function() {
+//     this.isAvailable = false;
+//     console.log(`"${this.title}" has been borrowed.`);
+//   },
+// };
+// console.log(book.isAvailable);
+// book.borrowBook();
+// console.log(book.isAvailable);
+
+// let person = {
+//   name: "Dogged",
+//   age: 30,
+//   city: "enugu",
+//   introduce: function () {
+//     console.log(
+//       `"Hi, I'm ${this.name}, I'm ${this.age} years old, and I live in ${this.city}`
+//     );
+//   },
+// };
+// person.introduce();
+var firstName = 'Chiedozie';
+console.log(firstName)
+
+var lastName = 'Chukwuma';
+console.log(lastName)
+
+var age = 120;
+console.log(age)
+
+var licenseStatus = true;
+console.log(licenseStatus)
+
+var dreamJob;
+console.log(dreamJob)
